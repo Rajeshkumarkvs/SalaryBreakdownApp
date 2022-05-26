d10b9882acba873e1f2fe2698178376375b3ef28
Rajeshkumarkvs <106056424+Rajeshkumarkvs@users.noreply.github.com>
2022-05-26 12:01:31 +1000

    Initial version of Salary breakdown application

---
+++ SalaryBreakdown/SalaryBreakdown/Extensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace SalaryBreakdownApp.SalaryPackage
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Round the decimal value to specified decimal places
+        /// </summary>
+        /// <param name="d">decimal number</param>
+        /// <param name="decimalPlaces">number of places to round up</param>
+        /// <returns>
+        /// <see cref="decimal"/> Rounded decimal value
+        /// </returns>
+        public static decimal Round(this decimal d, int decimalPlaces)
+        {
+            return Math.Round(d, decimalPlaces);
+        }
+
+        /// <summary>
+        /// Round up the decimal value to nearest dollar value
+        /// </summary>
+        /// <param name="d">decimal value that needs to be rounded up to the nearest dollar</param>
+        /// <returns>
+        /// A <see cref="decimal"/> that is rounded up to nearest dollar
+        /// </returns>
+        public static decimal RoundUpToDollar(this decimal d)
+        {
+            return Math.Ceiling(d);
+        }
+
+        /// <summary>
+        /// Round the decimal value to the nearest value
+        /// </summary>
+        /// <param name="d">decimal number</param>
+        /// <returns>
+        /// <see cref="decimal"/> Rounded decimal value to nearest value
+        /// </returns>
+        public static decimal Round(this decimal d)
+        {
+            return Math.Round(d);
+        }
+
+        /// <summary>
+        /// Round the decimal value to the nearest integral value that is less than or equal to the specified decimal value 
+        /// </summary>
+        /// <param name="d">decimal number</param>
+        /// <returns>
+        /// A <see cref="decimal"/> that is rounded to nearest integral value that is less than or equal to the specified decimal value
+        /// </returns>
+        public static decimal RoundDown(this decimal d)
+        {
+            return Math.Floor(d);
+        }
+
+        /// <summary>
+        /// Returns the decimal values in currency format
+        /// </summary>
+        /// <param name="d">decimal value that needs to displayed in currency format</param>
+        /// <returns>
+        /// A <see cref="string"/> that is formatted from decimal value
+        /// </returns>
+        public static string ToCurrency(this decimal d)
+        {
+            return d.ToString("C", CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// Represents function to check whether the string value is present in list of values
+        /// </summary>
+        /// <param name="s">
+        /// The <see cref="string"/> that needs to be validated
+        /// </param>
+        /// <param name="values">
+        /// <see cref="string[]"/> list of values to check
+        /// </param>
+        /// <returns>
+        /// A <see cref="bool"/> value. True if the string found in the list of values provided. Otherwise false.
+        /// </returns>
+        public static bool IsValidPayRequency(this string s)
+        {
+            List<string> validPayFrequency = new List<string> { "M", "W", "F" };
+
+            return validPayFrequency.Any(x => x == s);
+        }
+    }
+}
---
+++ SalaryBreakdown/SalaryBreakdownApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SalaryBreakdownApp.SalaryPackage;
+using System;
+
+namespace SalaryBreakdownApp
+{
+    class Program
+    {
+        private readonly ILogger<Program> _logger;
+        private readonly ISalaryBreakdown _salaryBreakdown;
+
+        static void Main(string[] args)
+        {
+            IHost host = CreateHostBuilder(args).Build();
+            host.Services.GetRequiredService<Program>().Run();
+        }
+
+        public Program(ILogger<Program> logger, ISalaryBreakdown salaryBreakdown)
+        {
+            _logger = logger;
+            _salaryBreakdown = salaryBreakdown;
+        }
+
+        public void Run()
+        {
+            Console.Write("Enter your salary package amount: ");
+            var grossAmountInput = Console.ReadLine();
+            decimal grossAmount;
+
+            //Validate the gross amount entered
+            if (!Decimal.TryParse(grossAmountInput, out grossAmount))
+            {
+                Console.WriteLine("\nThe salary package amount you entered is incorrect.");
+                return;
+            }
+
+            Console.Write("Enter your pay frequency (W for weekly, F for fortnightly, M for monthly): ");
+            string payFrequenceInput = Console.ReadLine();
+
+            //Validate the Pay frequency entered
+            if (!payFrequenceInput.IsValidPayRequency())
+            {
+                Console.WriteLine("\nThe Pay frequency you entered is incorrect.");
+                return;
+            }
+
+            //Convert input pay frequency to PayFrequency enum
+            var payFrequency = payFrequenceInput.ToUpper() == "M" ? PayFrequency.month : payFrequenceInput.ToUpper() == "W" ? PayFrequency.week : PayFrequency.fortnight;
+
+            Console.WriteLine("\nCalculating salary details...");
+
+            _salaryBreakdown.CalculateSalaryBreakdown(grossAmount, payFrequency);
+
+            Console.WriteLine($"\nGross package: {_salaryBreakdown.GrossIncome.ToCurrency()}");
+            Console.WriteLine($"Superannuation: {_salaryBreakdown.Superannuation.ToCurrency()}");
+            Console.WriteLine($"\nTaxable income: {_salaryBreakdown.TaxableIncome.ToCurrency()}");
+
+            Console.WriteLine("\nDeductions:");
+            Console.WriteLine($"Medicare Levy: {_salaryBreakdown.MedicareLevy.ToCurrency()}");
+            Console.WriteLine($"Budget Repair Levy: {_salaryBreakdown.BudgetRepairLevy.ToCurrency()}");
+            Console.WriteLine($"Income Tax: {_salaryBreakdown.IncomeTax.ToCurrency()}");
+
+            Console.WriteLine($"\nNet income: {_salaryBreakdown.NetIncome.ToCurrency()}");
+            Console.WriteLine($"Pay packet: {_salaryBreakdown.PayBucket.ToCurrency()} per {payFrequency.ToString()}");
+
+            Console.WriteLine("\nPress any key to end...");
+            Console.ReadKey();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((configuration) =>
+                {
+                    configuration.Sources.Clear();
+                    configuration
+                        .AddJsonFile("TaxRules.json", optional: false, reloadOnChange: true)
+                        .Build();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddTransient<Program>();
+                    services.AddTransient<ISalaryBreakdown, SalaryBreakdown>();
+                    services.AddTransient<TaxBracketRules>();
+                });
+        }
+    }
+}
